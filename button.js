46a29a4a4f7fa5f8a171ccf699184106c130d5f5
ToastMaster <kgranger777@gmail.com>
2020-06-09 13:36:49 -0700

    Add External JS files
    - Added button.js to handle button presses
    - Updated index.html to include and use external button.js file
    - Removed internal JavaScript

---
+++ button.js
@@ -0,0 +1,8 @@
+function buttonPressed() {
+    var x = document.getElementById("pressButtonAction");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
