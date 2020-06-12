f4d2d4d7904bc7ec06be5430220431dbff3e8349
ToastMaster <kgranger777@gmail.com>
2020-06-11 22:19:46 -0700

    Hour3 Final Commit
    Fixed Hour 3's operation

--- cmpsci190/Hour3/hour3.js
+++ cmpsci190/Hour3/hour3.js
@@ -2,26 +2,14 @@
 var input = document.getElementById('inputField');
 var output = document.getElementById('outputField');
 
-// this function updates the output when it is called
-function sendOutput(input) {
-    console.log(input);
-    if (isNaN(input.target.value)) {
-        // if input is not a number, output invalid message to innerHTML
-        output.innerHTML = "Invalid input.";
-    } else if(input.target.value === "") {
-        // if input is not received, output default text (nothing)
-        output.innerHTML = "";
-    } else {
-        // if input is received, run convert function and output to innerHTML
-        output.innerHTML = convert(input.target.value);
-    }
+function sendOutput() {
+    // printOutput retrieves the converted value
+    var printOutput = convert(input.value);
+    // this sets innerHTML of output object (outputField in HTML) to the converted value
+    output.innerHTML = printOutput.toFixed(2) + "&#186F";
 }
 
 function convert(input) {
     // returns input converted to Fahrenheit using the formula
-    return (input * 9/5) + 32 + "&#186F";
+    return (input * (9 / 5)) + 32;
 }
-
-// add new EventListener to continuously listen for user input into text field
-// calls sendOutput when input is detected
-input.addEventListener("input", sendOutput);
